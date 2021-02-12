ec5cd53be96870d40c4518a527ade10fc0198015
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-02-12 23:08:16 +0000

    Main Class created(dummy)

---
+++ src/nvhc/Main.java
@@ -0,0 +1,16 @@
+package nvhc;
+
+
+
+public class Main {
+	
+
+	//This is the main class
+public static void main(String[] args) {
+	
+	
+	
+	}
+}
+
+
---
+++ src/nvhc/Nvhc.java
@@ -0,0 +1,5 @@
+package nvhc;
+
+public class Nvhc {
+
+}
